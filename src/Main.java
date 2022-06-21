8e30ae944495ea7659588981ce2e3c5f0b6859ba
kaninaoksana <oksana_1710@mail.ru>
2022-06-21 22:29:26 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int price = 10000;
         int bonus = price / 20;
-                System.out.println("Боyусные мили:"+ bonus);
-
+        System.out.println("Бонусные мили:" + bonus);
 
 
     }
-    }
+}
